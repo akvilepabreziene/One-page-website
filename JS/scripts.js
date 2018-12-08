221a361ebfba0d0cf91dc426d59df36e4113032f
akvilepabreziene <44781334+akvilepabreziene@users.noreply.github.com>
2018-12-08 22:03:39 +0200

    puslapio papildymas

---
+++ JS/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+        var scrollLink = $('.scroll');
+
+        scrollLink.click(function(e) {
+
+            e.preventDefault();
+            $('body, html').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 1000 );
+        });
+   
+}); 
+
+// kontaktų formos tikrinimas ar užpildyti laukeliai
+
+$('form').submit(function( event ) {
+    event.preventDefault();
+});
+
+$('form .btn-info').click(function(){
+
+   var name = $('#name').val();
+   var email = $('#email').val();
+   var message = $('#message').val();
+
+   if(name == "" || email == "" || message == ""){
+      
+        $('#alertMessage').modal('show');  
+   }
+  
+
+});
+
+// Paslaugų skilties papildomos informacijos atidarymas su mygtuku 'Daugiau'
+$('#info1').hide();
+$('#info2').hide();
+$('#info3').hide();
+
+
+$('#btn1').click(function(){
+    $('#info1').fadeIn(500);
+}).dblclick(function(){
+    $('#info1').hide();
+});
+
+$('#btn2').click(function(){
+    $('#info2').fadeIn(500);
+}).dblclick(function(){
+    $('#info2').hide();
+});
+
+$('#btn3').click(function(){
+    $('#info3').fadeIn(500);
+}).dblclick(function(){
+    $('#info3').hide();
+});
+
