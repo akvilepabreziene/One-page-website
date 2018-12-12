407f69d91f6eb18a4b3167a4884f28e992454179
akvilepabreziene <44781334+akvilepabreziene@users.noreply.github.com>
2018-12-12 20:54:52 +0200

    Kodo peržiūrėjimas ir klaidų taisymas

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,44 +9,42 @@ $(document).ready(function() {
                 scrollTop: $(this.hash).offset().top
             }, 1000 );
         });
-   
+
+
+    // kontaktų formos tikrinimas ar užpildyti laukeliai
+
+    $('form').submit(function( event ) {
+        event.preventDefault();
+    });
+
+    $('form .btn-info').click(function(){
+
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
+
+        if(name == "" || email == "" || message == ""){
+            
+                $('#alertMessage').modal('show');  
+            }
+    });
+
+    // Paslaugų skilties papildomos informacijos atidarymas su mygtuku 'Daugiau'
+    $('#info1').hide();
+    $('#info2').hide();
+    $('#info3').hide();
+
+
+    $('#btn1').click(function(){
+        $('#info1').toggle();
+    });
+
+    $('#btn2').click(function(){
+        $('#info2').toggle();
+    });
+
+    $('#btn3').click(function(){
+        $('#info3').toggle();
+    });
+
 }); 
-
-// kontaktų formos tikrinimas ar užpildyti laukeliai
-
-$('form').submit(function( event ) {
-    event.preventDefault();
-});
-
-$('form .btn-info').click(function(){
-
-   var name = $('#name').val();
-   var email = $('#email').val();
-   var message = $('#message').val();
-
-   if(name == "" || email == "" || message == ""){
-      
-        $('#alertMessage').modal('show');  
-   }
-  
-
-});
-
-// Paslaugų skilties papildomos informacijos atidarymas su mygtuku 'Daugiau'
-$('#info1').hide();
-$('#info2').hide();
-$('#info3').hide();
-
-
-$('#btn1').click(function(){
-    $('#info1').toggle();
-});
-
-$('#btn2').click(function(){
-    $('#info2').toggle();
-});
-
-$('#btn3').click(function(){
-    $('#info3').toggle();
-});
-
